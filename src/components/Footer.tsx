a0727b930592689c01f97a61e69195a94e941592
Oliver Still <56825413+ojhstill@users.noreply.github.com>
2025-03-10 21:38:00 +0000

    nojekyll

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,8 +4,8 @@ export default function Footer() {
   return (
     <footer className="text-center pt-12">
       <p className="text-xs text-muted-foreground">
-        © {currentYear} Oliver Still. All rights reserved. Made using Vite,
-        React & Tailwind.
+        © {currentYear} Oliver Still. All rights reserved. Made using Vite +
+        React + TS + Tailwind.
       </p>
     </footer>
   );
