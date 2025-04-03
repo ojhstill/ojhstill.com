25c37fa0908ea1c54976b7bd713178bc9389b9d0
Oliver Still <56825413+ojhstill@users.noreply.github.com>
2025-04-03 22:31:01 +0100

    Wattle Image

--- src/pages/Journey.tsx
+++ src/pages/Journey.tsx
@@ -19,8 +19,8 @@ const data = [
         </div>
         <div className="grid grid-cols-2 gap-4">
           <img
-            src="https://media.licdn.com/dms/image/v2/D4D22AQHnfI0aaRtaFg/feedshare-shrink_2048_1536/feedshare-shrink_2048_1536/0/1684398209908?e=1744848000&v=beta&t=qvkFXEakV2AqV0dEfjbwg1bIbjGFoE9Ooe4tctAIuJU"
-            alt="Wattle Conference Stand"
+            src="https://media.licdn.com/dms/image/v2/D4E22AQHIJF8gEvV2JQ/feedshare-shrink_2048_1536/feedshare-shrink_2048_1536/0/1731325283879?e=1746662400&v=beta&t=KZ5fmSFQCKzJ8ktXIm_Q4JRfBhZwmwJjlKBhI8gX0es"
+            alt="Wattle Conference Room"
             width={500}
             height={500}
             className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
